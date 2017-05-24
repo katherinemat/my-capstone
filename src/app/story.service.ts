bd7e1521befc3ddf43f3d8bb76d7740afcf477b0
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-05-24 15:49:03 -0700

    select values from api return data that will constitute story object.

--- src/app/story.service.ts
+++ src/app/story.service.ts
@@ -12,7 +12,14 @@ export class StoryService {
     // change this url when project is deployed to the actual url
     axios.get('/api/stories')
       .then((res) => {
-        console.log(res.data);
+        for (var i = 0; i < res.data.stories.length; i++) {
+          var story = res.data.stories[i];
+          console.log(story.title);
+          console.log(story.author.name);
+          console.log(story.body);
+          console.log(story.links.permalink);
+          console.log(story.publishedAt);
+        }
         // knex.schema.table('stories', function(table) {
         //   table.
         //   some function that adds data to rows
