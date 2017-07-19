c4657f4c691a99a27d84f0a3e8431344d3f1667b
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-07-19 14:11:37 -0700

    create scatterplot that maps subject age, evenly spaced out horizontally

--- src/app/bar/bar.component.ts
+++ src/app/bar/bar.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { OfficerInvolvedShooting } from '../officer-involved-shooting.model';
 import * as d3 from 'd3';
 
-
 @Component({
   selector: 'app-bar',
   templateUrl: './bar.component.html',
@@ -19,66 +18,69 @@ export class BarComponent implements OnInit {
 
   test() {
     console.log(this.OfficerInvolvedShootingsGraphData);
-    this.d3Svg();
+    this.d3SvgBar();
   }
 
-  d3Svg() {
+  d3SvgBar() {
     var w = 500;
     var h = 1600;
     var barPadding = 1;
-    var testData = this.OfficerInvolvedShootingsGraphData;
+    //declared locally because some d3 functions can't reach all the way out to component properties
+    var graphData = this.OfficerInvolvedShootingsGraphData;
 
     var svg = d3.select("#summary-length-graph")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
 
     svg.selectAll("rect")
-      .data(testData)
-      .enter()
-      .append("rect")
-      .attr("x", 0)
-      .attr("y", function(d, i) {
-        // return w - (d * 4);
-        return i * (h / testData.length);
-      })
-      .attr("width", function(d) {
-        return d.summary.length / 10;
-      })
-      .attr("height", function() {
-        return h / testData.length - barPadding;
-      })
-      .attr("fill", "teal");
+        .data(graphData)
+        .enter()
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", function(d, i) {
+          // return w - (d * 4);
+          return i * (h / graphData.length);
+        })
+        .attr("width", function(d) {
+          return d.summary.length / 10;
+        })
+        .attr("height", function() {
+          return h / graphData.length - barPadding;
+        })
+        .attr("fill", "teal");
 
     svg.selectAll("text")
-      .data(testData)
-      .enter()
-      .append("text")
-      .text(function(d, i) {
-        return d.summary.length;
-      })
-      .attr("x", 0)
-      .attr("y", function(d, i) {
-        return (i + 1) * (h / testData.length);
-      });
+        .data(graphData)
+        .enter()
+        .append("text")
+        .text(function(d, i) {
+          return d.summary.length;
+        })
+        .attr("x", function(d) {
+          return d.summary.length / 10;
+        })
+        .attr("y", function(d, i) {
+          return (i + 1) * (h / graphData.length);
+        });
 
     // svg.selectAll("circle")
-    //   .data(this.testData)
-    //   .enter()
-    //   .append("circle")
-    //   //i is index position, d is piece of data
-    //   .attr("cx", function(d, i) {
-    //     return (i * 50) + 25;
-    //   })
-    //   .attr("cy", h/2)
-    //   .attr("r", function(d) {
-    //     return d;
-    //   })
-    //   .attr("fill", "yellow")
-    //   .attr("stroke", "orange")
-    //   .attr("stroke-width", function(d) {
-    //       return d/2;
-    //   });
+      //   .data(this.graphData)
+      //   .enter()
+      //   .append("circle")
+      //   //i is index position, d is piece of data
+      //   .attr("cx", function(d, i) {
+      //     return (i * 50) + 25;
+      //   })
+      //   .attr("cy", h/2)
+      //   .attr("r", function(d) {
+      //     return d;
+      //   })
+      //   .attr("fill", "yellow")
+      //   .attr("stroke", "orange")
+      //   .attr("stroke-width", function(d) {
+      //       return d/2;
+      //   });
   }
 
   d3Html() {
--- src/app/scatterplot/scatterplot.component.ts
+++ src/app/scatterplot/scatterplot.component.ts
@@ -10,9 +10,51 @@ import * as d3 from 'd3';
 export class ScatterplotComponent implements OnInit {
   @Input() OfficerInvolvedShootingsGraphData: OfficerInvolvedShooting[];
 
+  public testData = [5, 10, 13, 19, 11];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  test() {
+    this.d3SvgScatterplot();
+  }
+
+  d3SvgScatterplot() {
+    var h = 500;
+    var w = 800;
+    var graphData = this.OfficerInvolvedShootingsGraphData;
+
+    var svg = d3.select("#subject-age-graph")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+    svg.selectAll("circle")
+       .data(graphData)
+       .enter()
+       .append("circle")
+       .attr("cx", function(d, i) {
+         return i * (w / graphData.length);
+       })
+       .attr("cy", function(d) {
+         return h - (d.subjectAge * 4);
+       })
+       .attr("r", 5);
+
+    svg.selectAll("text")
+       .data(graphData)
+       .enter()
+       .append("text")
+       .text(function(d) {
+         return d.subjectAge;
+       })
+       .attr("x", function(d, i) {
+         return i * (w / graphData.length);
+       })
+       .attr("y", function(d) {
+         return h - (d.subjectAge * 4);
+       })
+  }
 }
