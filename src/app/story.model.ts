59c8f8755d778ad3f1fd413ecb9f79a9391923fc
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-08-07 10:32:49 -0700

    remove uneccessary files.

--- src/app/story.model.ts
+++
@@ -1,3 +0,0 @@
-export class Story {
-  constructor(public source: string, public title: string, public author: string, public date: string, public link: string, public img: string, public content: string) { }
-}
